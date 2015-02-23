8e70c858cf0608ec58053dc323ab774a6ecec93c
jackfjo3 <jackfjo3@gmail.com>
2015-02-22 18:55:51 -0700

    login page and initial authentication. up to 8.1.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
-  root              'static_pages#home'
+  get 'sessions/new'
 
-  get 'help'    =>  'static_pages#help'
+  root                  'static_pages#home'
 
-  get 'about'   =>  'static_pages#about'
+  get     'help'    =>  'static_pages#help'
+
+  get     'about'   =>  'static_pages#about'
   
-  get 'contact' =>  'static_pages#contact'
+  get     'contact' =>  'static_pages#contact'
   
-  get 'signup'  =>  'users#new'
+  get     'signup'  =>  'users#new'
+
+  get     'login'   =>  'sessions#new'
+
+  post    'login'   =>  'sessions#create'
+
+  delete  'logout'  =>  'sessions#destroy'
   
   resources :users
 
