7fc7b5e3169fed67af9e5e26139f2f9cd4dc8841
jackfjo3 <jackfjo3@gmail.com>
2015-05-15 14:10:19 -0600

    micropost validation for file size. add AWS for production image storage (hopefully works...)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,17 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  mount_uploader :picture, PictureUploader
   default_scope -> { order(created_at: :desc)}
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate 	:picture_size # note validate not validates for custom validation
+private
+
+	# Validates size of uploaded picture
+	def picture_size
+		if picture.size > 5.megabytes
+			error.add(:picture, "should be less than 5 mb")
+		end
+	end
 
 end
